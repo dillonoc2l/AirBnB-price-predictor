538e6f477c5c56f3c5b2e307e74244753a37a5a5
dillonoc2l <Dillonco2L@gmail.com>
2025-12-03 11:01:26 +0000

    Add files via upload

--- AirBnB_price_predictions.py
+++ AirBnB_price_predictions.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np 
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn import tree
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import accuracy_score, pairwise_distances, r2_score, mean_absolute_error
 
 
 df = pd.read_csv("clean_listings.csv")
@@ -31,20 +31,93 @@ df = pd.concat([df, oheRoomType], axis = 1).drop(columns = ['room_type'])       
 '''
 
 
+def score_model(df_check):
+    X = df_check.drop(['price', 'id', 'host_id'],  axis=1)#create features
+    y = df_check["price"]#create target  
 
-X = df.drop(['price', 'id', 'host_id'],  axis=1)#create features
-y = df["price"]#create target  
+
+
+    #test and train normlly for testing purposes:
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=32
+    )
+    model =  GradientBoostingRegressor()
+    model = model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+
+    # Evaluate
+    r2 = r2_score(y_test, y_pred)
+    print("R2 Score:", r2)
+
+    #cross validation
+    cvScore = cross_val_score(model, X, y.values.ravel(), cv = 5)
+
+    print('average CV Score:', np.mean(cvScore))
+
+
+print('Original:')
+score_model(df)
+    
+
+
+
+''' #gridsearch for best hyperparameters
+param_grid = [{ 
+    'max_depth' : [3,4,5,6], 
+    'learning_rate' : [0.1, 0.2]
+    #'validation_fraction' : [0.1,0.3,0.5]
+    }]
+
+grid_search = GridSearchCV(model, param_grid, cv=3, scoring = 'r2', n_jobs=-1 )
+
+grid_search.fit(X_train,y_train)
+
+print(grid_search.best_score_)
+print(grid_search.best_params_)
+print(grid_search.cv_results_)
+
 '''
-clf = tree.DecisionTreeRegressor()
-clf = clf.fit(X,y)
 
-cvscore = cross_val_score(clf, X, y.values.ravel(), cv = 10)
 
-print(cvscore)
-'''
-model = GradientBoostingRegressor()#RandomForestRegressor(verbose=1, n_jobs=-1)
-#model = model.fit(X,y)
+#Test to see if addeding new parameter 'distance to city centre' imporves models predictions
 
-cvScore = cross_val_score(model, X, y.values.ravel(), cv = 10)
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371
+    lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2)**2
+    return 2 * R * np.arcsin(np.sqrt(a))
 
-print(cvScore)
+center_lat, center_lon = 51.509865, -0.118092
+
+
+df_added_centre = df.copy()
+
+df_added_centre['dist_to_center'] = haversine(
+    df_added_centre['latitude'], df_added_centre['longitude'],
+    center_lat, center_lon
+)
+
+df_added_centre.drop(columns="latitude", axis = 1, inplace=True)
+df_added_centre.drop(columns="longitude", axis = 1, inplace=True)
+
+print('Added distance to centre:')
+score_model(df_added_centre)#score returned is worse tha original
+
+df = df_added_centre
+
+df_dropped = df.copy()
+
+#print(df.columns)
+df_dropped.drop(columns="reviews_per_month", axis = 1, inplace=True)
+
+print('Removed reviews_per_month:')
+score_model(df_dropped)
+
+df_dropped.drop(columns="number_of_reviews_ltm", axis = 1, inplace=True)
+
+print('Removed number_of_reviews_ltm:')
+score_model(df_dropped)
